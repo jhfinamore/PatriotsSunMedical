9a9e5646fe1c6037ccf1ee48047f44f3dfe6fd21
Tyler Lopez <trlopez@coastal.edu>
2018-11-13 19:04:38 -0500

    Update PatriotsSunMedical/backend/models/patient.js
    I dont think anything really changes here, but its working on my machine.

--- PatriotsSunMedical/backend/models/patient.js
+++ PatriotsSunMedical/backend/models/patient.js
@@ -1,8 +1,4 @@
-// Had an issue getting import or export to work, if any of you back-end dudes know how to fix this feel free
 import mongoose from 'mongoose';
-
-// In the mean time this seems to work alrighty
-// const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
@@ -30,6 +26,4 @@ let Patient = new Schema({
     }
 });
 
-// Also had a problem with this one, but commonJS syntax seemed to work it out. Absolutely no idea why 
 export default mongoose.model('Patient', Patient);
-// module.exports = mongoose.model('Patient', Patient);
